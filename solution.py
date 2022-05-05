74749e067a8d428386a229cf138fefa3c76cd217
Thomas <me>
2022-05-06 00:26:07 +0200

    beautify

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import json
 from datetime import datetime
 import numbers
 
-# Path to file on my home (windows) machine.
+# Path to 1_day file on my home (windows) machine.
 datafile ="D:\\em\\1_day.jsonl";
 
 def sumProd(production):
@@ -15,16 +15,15 @@ def getTimestamp(thjson):
     dt = thjson["datetime"]
     parsedtime = datetime.fromisoformat(dt)
     return int(parsedtime.timestamp())
+
 def calculateConsumption(prodexchange):
     return prodexchange['production'][0] -prodexchange['export'][0] if prodexchange['export'] else prodexchange['production'][0] 
-
 
 def getFlow(flowdata):
   flow = flowdata["sortedCountryCodes"]
   items = []
-  # Assuming alll flows go either to or from a zone starting with DK
-
-   # "Negative values indicate export of electricity out of the area to the connected area, and positive values indicate import."
+  # Assuming alll flows go either to or from a zone starting with DK.
+  # "Negative values indicate export of electricity out of the area to the connected area, and positive values indicate import."
   if "->DK" in flow:
     items.append( ( flow.split("->")[1], -1.0*flowdata["netFlow"]  ) ) 
   if flow.startswith("DK"):
@@ -38,7 +37,6 @@ class AddTimestampDoFn(beam.DoFn):
 
 class BuildRecordFn(beam.DoFn):
   def process(self, element,  window=beam.DoFn.WindowParam):
-    #window_start = window.start.to_utc_datetime()
     window_end = window.end.to_utc_datetime()
     yield (element[0], element[1], window_end )
 
@@ -63,7 +61,7 @@ netExchangeDkPerWindow =   netExchangeDk | 'Group and sum exchange' >> beam.Comb
 prodexchange = ( {'production':windowedProductionByZone, 'export': netExchangeDkPerWindow}) | 'Merge' >> beam.CoGroupByKey()
 withtimes = prodexchange |  'AddWindowEndTimestamp' >> (beam.ParDo(BuildRecordFn())) 
 consumption = withtimes | 'calculate consumption' >> beam.Map(lambda e: ( e[0], calculateConsumption(e[1]), e[2]  ) ) 
-printed = consumption | 'print' >>  beam.Map(lambda e: print( str(e[0]) + "\t"+ str(e[2]) +"\t" + f'{e[1]:.2f}'  ))
+printed = consumption | 'print' >>  beam.Map(lambda e: print( str(e[0]) + "\t" + str(e[2]) + "\t" + f'{e[1]:.2f}'  ))
 
 p.run()
 
